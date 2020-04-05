89691774f66b977b36b4852aed59b44d53393841
braver90 <aldoderosa90@gmail.com>
2020-04-05 20:43:13 +0200

    4 files modified

--- components/VariableFormatter/VariableFormatter.js
+++ components/VariableFormatter/VariableFormatter.js
@@ -1,23 +1,29 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import './VariableFormatter.css'
 
 
 const VariableFormatter = props =>{
   const [value,setValue] = useState("");
   const [formattedValue,setFormatted] = useState("");
-
+  const textRef = useRef();
 const handleChange = evt =>
 {
   const val = evt.target.value;
   setValue(val);
 }
+const reset = evt =>
+{
+  setValue("");
+  setFormatted("");
+  textRef.current.value="";
+}
   const format = ()=>
   {
     const aggrFunc = ["sum","avg","max","min"];
     const apexFunc = ["date","substring","substr","num"];
-    const Reg$ = /(\$)/gi
+    const Reg$ = /\$/gi
     let text = value;
-    text = text.indexOf("$")>-1?text.replace(Reg$,`'&'\$'&'`):text;
+    text = text.indexOf("$")>-1?text.replace(Reg$,"'\&'\ $ '\&'"):text;
     setFormatted(text);
     
   }
@@ -25,13 +31,14 @@ const handleChange = evt =>
   return(
     <React.Fragment>
     <div className={"var-input"}>
-        <textarea onChange = {(e)=>handleChange(e)}></textarea>
+        <textarea ref={textRef} onChange = {(e)=>handleChange(e)}></textarea>
+        <button className={"button-prm"} onClick={reset}>Reset</button>
         {value===""?<button className="disabled-btn" disabled>Formatta Variabile</button>
         :<button className={"button-prm"} onClick={format}>Formatta Variabile</button>}
     </div>
     
-    {formattedValue===""?<p className={["var-output","Closed"].join(" ")}>{formattedValue}</p>
-    :<p className={["var-output","Open"].join(" ")}>{formattedValue}</p>}
+    {formattedValue===""?<div className={["var-output","Close"].join(" ")}>{formattedValue}</div>
+    :<div className={["var-output","Open"].join(" ")}><p>{formattedValue}</p></div>}
 
     </React.Fragment>
   )
