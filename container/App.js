2c768a85766f7fdeed7eaab3c0008596bdca3e26
braver90 <aldoderosa90@gmail.com>
2020-04-05 19:08:20 +0200

    2 files modified, 2 added

---
+++ container/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+
+const App = props =>{
+  return(
+    <div className={"container"}>
+    <h1>Qlik Variable Formatter</h1>
+    </div>
+  )
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import App from './container/App';
 import './style.css';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          App di prova per firebase
-        </p>
-      </div>
-    );
-  }
-}
 
 render(<App />, document.getElementById('root'));
